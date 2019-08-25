d16aec305aa125dd7260d5dd7b721853ce4f7fbc
slorber <lorber.sebastien@gmail.com>
2019-08-25 19:49:50 +0200

    style mdx button + add prettier

--- content/posts/2019-08-20-my-first-post-using-novela-by-narative/components.jsx
+++ content/posts/2019-08-20-my-first-post-using-novela-by-narative/components.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React from 'react';
+import { withTheme } from 'emotion-theming';
 
-export const TestMDXButton = () => (
-  <div style={{display: "flex",justifyContent: "center"}}>
-    <button
-      onClick={() => alert("it works")}
-      style={{border: "solid", borderRadius: 5, padding: 10}}
-    >
-      Click me!
-    </button>
-  </div>
-);
+export const TestMDXButton = withTheme(({ theme: { colors } }) => {
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <button
+        onClick={() => alert('it works')}
+        css={{
+          border: 'solid',
+          borderColor: colors.articleText,
+          color: colors.articleText,
+          borderRadius: 10,
+          padding: 10,
+          marginBottom: 30,
+        }}
+      >
+        Click me!
+      </button>
+    </div>
+  );
+});
