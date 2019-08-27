501a8eda7ce078987524c6882e21a480b21fe361
slorber <lorber.sebastien@gmail.com>
2019-08-27 11:48:18 +0200

    concurrency

--- content/posts/2019-08-25-handling-request-concurrency-in-react/components.jsx
+++ content/posts/2019-08-25-handling-request-concurrency-in-react/components.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styled from '@emotion/styled';
-import { withTheme } from 'emotion-theming';
+import { sample } from 'lodash';
 
-const fetchStarwarsHeroData = async id => {
-  const result = await fetch(`https://swapi.co/api/people/${id}/`);
+const fetchStarwarsHeroData = async (id, options) => {
+  const result = await fetch(`https://swapi.co/api/people/${id}/`, options);
   if (result.status !== 200) {
     throw new Error('bad status = ' + result.status);
   }
@@ -48,24 +48,7 @@ const SliderContainer = styled.div({
   padding: 10,
 });
 
-const StarwarsSlider = ({ component: Component }) => {
-  const [id, setId] = useState(1);
-  return (
-    <SliderContainer>
-      <SliderArrowButton onClick={() => setId(i => (i > 2 ? i - 1 : i))}>
-        {'<'}
-      </SliderArrowButton>
-      <SliderContent>
-        <Component id={id} />
-      </SliderContent>
-      <SliderArrowButton onClick={() => setId(i => i + 1)}>
-        {'>'}
-      </SliderArrowButton>
-    </SliderContainer>
-  );
-};
-
-const StarwarsHeroSlideContent = ({ id, data }) => (
+const SlideContent = ({ id, data }) => (
   <div>
     <div>Id={id}</div>
     <div css={{ marginTop: 10 }}>
@@ -78,8 +61,47 @@ const StarwarsHeroSlideContent = ({ id, data }) => (
   </div>
 );
 
-const StarwarsHeroBase = ({ id }) => {
+const useStarwarsSliderState = () => {
+  const [id, setId] = useState(1);
+
+  const api = useMemo(() => {
+    return {
+      previous: () => setId(i => (i > 2 ? i - 1 : i)),
+      next: () => setId(i => i + 1),
+    };
+  }, [setId]);
+
+  return [id, api];
+};
+
+const StarwarsSlider = ({ id, data, previous, next }) => {
+  return (
+    <SliderContainer>
+      <SliderArrowButton onClick={() => previous()}>{'<'}</SliderArrowButton>
+      <SliderContent>
+        <SlideContent id={id} data={data} />
+      </SliderContent>
+      <SliderArrowButton onClick={() => next()}>{'>'}</SliderArrowButton>
+    </SliderContainer>
+  );
+};
+
+const delayRandomly = () => {
+  const timeout = sample([0, 200, 500, 700, 1000, 3000]);
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
+const throwRandomly = () => {
+  const shouldThrow = sample([true, false, false]);
+  if (shouldThrow) {
+    throw new Error('simulated async failure');
+  }
+};
+
+export const StarwarsHeroSliderDefault = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
   const [data, setData] = useState(null);
+
   useEffect(() => {
     setData(null);
     fetchStarwarsHeroData(id).then(
@@ -87,11 +109,152 @@ const StarwarsHeroBase = ({ id }) => {
       e => console.warn('fetch failure', e),
     );
   }, [id]);
-  return <StarwarsHeroSlideContent id={id} data={data} />;
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
 };
 
-export const StarwarsHeroBaseSlider = withTheme(({ theme: { colors } }) => (
-  <div css={{ display: 'flex', justifyContent: 'center' }}>
-    <StarwarsSlider component={StarwarsHeroBase} />
-  </div>
-));
+export const StarwarsHeroSliderDelay = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+    fetchStarwarsHeroData(id)
+      .then(async data => {
+        await delayRandomly();
+        return data;
+      })
+      .then(result => setData(result), e => console.warn('fetch failure', e));
+  }, [id]);
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
+};
+
+export const StarwarsHeroSliderThrow = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+    fetchStarwarsHeroData(id)
+      .then(data => {
+        throwRandomly();
+        return data;
+      })
+      .then(result => setData(result), e => console.warn('fetch failure', e));
+  }, [id]);
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
+};
+
+export const StarwarsHeroSliderDelayThrow = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+    fetchStarwarsHeroData(id)
+      .then(async data => {
+        await delayRandomly();
+        throwRandomly();
+        return data;
+      })
+      .then(result => setData(result), e => console.warn('fetch failure', e));
+  }, [id]);
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
+};
+
+export const StarwarsHeroSliderIgnoring = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
+  const [data, setData] = useState(null);
+
+  const lastPromise = useRef();
+
+  useEffect(() => {
+    setData(null);
+    const currentPromise = fetchStarwarsHeroData(id).then(async data => {
+      await delayRandomly();
+      throwRandomly();
+      return data;
+    });
+
+    lastPromise.current = currentPromise;
+
+    currentPromise.then(
+      result => {
+        if (currentPromise === lastPromise.current) {
+          setData(result);
+        }
+      },
+      e => {
+        if (currentPromise === lastPromise.current) {
+          console.warn('fetch failure', e);
+        }
+      },
+    );
+  }, [id]);
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
+};
+
+export const StarwarsHeroSliderAborting = () => {
+  const [id, { previous, next }] = useStarwarsSliderState();
+  const [data, setData] = useState(null);
+
+  // Store abort controller which will permit to abort the last issued request
+  const lastAbortController = useRef();
+
+  useEffect(() => {
+    setData(null);
+
+    // When a new request is going to be issued, the first thing to do is cancel the previous request
+    if (lastAbortController.current) {
+      lastAbortController.current.abort();
+    }
+
+    // Create new AbortController for the new request and store it in the ref
+    const currentAbortController = new AbortController();
+    lastAbortController.current = currentAbortController;
+
+    // Issue the new request, that may eventually be aborted by a subsequent request
+    const currentPromise = fetchStarwarsHeroData(id, {
+      signal: currentAbortController.signal,
+    }).then(data => {
+      // You might notice I had to remove the bad network simulation code:
+      // this is because it's not possible to abort this part of the code with the abort signal
+      return data;
+    });
+
+    currentPromise.then(
+      result => setData(result),
+      e => console.warn('fetch failure', e),
+    );
+  }, [id]);
+
+  return (
+    <div css={{ display: 'flex', justifyContent: 'center' }}>
+      <StarwarsSlider id={id} data={data} previous={previous} next={next} />
+    </div>
+  );
+};
