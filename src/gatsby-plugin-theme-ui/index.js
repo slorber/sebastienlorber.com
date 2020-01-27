0176ef2ad567fadad0602f5027e78d959ead1efc
slorber <lorber.sebastien@gmail.com>
2020-01-27 17:27:55 +0100

    try to add theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,14 +5,9 @@ import novelaTheme from '@narative/gatsby-theme-novela/src/gatsby-plugin-theme-u
 
 const theme = {
   ...novelaTheme,
-  // initialColorMode: `dark`,
+  initialColorMode: `dark`,
   colors: {
     ...novelaTheme.colors,
-    //primary: '#000',
-    //secondary: '#73737D',
-    //accent: '#6166DC',
-    //grey: '#73737D',
-    //background: '#fff',
   },
 };
 
