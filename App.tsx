2f43df1fa50ab1dd4721960d4bd54f1f15154f97
slorber <lorber.sebastien@gmail.com>
2020-02-03 13:20:00 +0100

    create AppBlogPostScreen

--- App.tsx
+++ App.tsx
@@ -3,8 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MDXProvider } from '@mdx-js/react';
 
-import AppNavigator from './AppNavigator';
 import AppMDXComponents from './AppMDXComponents';
+import AppBlogPostScreen from './AppBlogPostScreen';
+
+
+const AppNavigator = () => {
+  return (
+    <AppBlogPostScreen
+      mdxBlogPostRequire={require('./content/posts/2019-08-20-my-first-post-using-novela-by-narative/index.mdx')}
+      //mdxBlogPostRequire={require('./content/posts/2019-08-30-handling-race-conditions-in-react/index.mdx')}
+      //mdxBlogPostRequire={require('./content/posts/2020-01-27-running-expo-inside-a-gatsby-blog-post/index.mdx')}
+    />
+  );
+};
+
 
 const App = () => {
   return (
--- AppBlogPostScreen.tsx
+++ AppBlogPostScreen.tsx
@@ -1,14 +1,13 @@
 import React, { lazy, Component, Suspense } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
-import TestMDXScreen, {
-  _frontmatter,
-} from './content/posts/2019-08-20-my-first-post-using-novela-by-narative/index.mdx';
 import AutoHeightImage from 'react-native-auto-height-image';
 import { useSafeArea } from 'react-native-safe-area-context';
 
 const ScreenWidth = Dimensions.get('window').width;
 
-const AppNavigator = () => {
+const AppBlogPostScreen = ({ mdxBlogPostRequire }) => {
+  const MDXBlogPostComp = mdxBlogPostRequire.default;
+  const frontmatter = mdxBlogPostRequire._frontmatter;
   const insets = useSafeArea();
   return (
     <ScrollView style={{ flex: 1, marginTop: insets.top }}>
@@ -20,7 +19,19 @@ const AppNavigator = () => {
           justifyContent: 'center',
         }}
       >
-        <AutoHeightImage width={ScreenWidth - 40} source={_frontmatter.hero} />
+        <Text style={{ fontSize: 30, fontWeight: 'bold' }}>
+          {frontmatter.title}
+        </Text>
+      </View>
+      <View
+        style={{
+          marginTop: 20,
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <AutoHeightImage width={ScreenWidth - 40} source={frontmatter.hero} />
       </View>
       <View
         style={{
@@ -30,11 +41,11 @@ const AppNavigator = () => {
         }}
       >
         <View style={{ width: ScreenWidth - 40, overflow: 'hidden' }}>
-          <TestMDXScreen />
+          <MDXBlogPostComp />
         </View>
       </View>
     </ScrollView>
   );
 };
 
-export default AppNavigator;
+export default AppBlogPostScreen;
