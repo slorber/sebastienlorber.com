be434d622588d1a734e2d61c870f171e21a0e766
slorber <lorber.sebastien@gmail.com>
2020-02-28 16:37:59 +0100

    add sponsor url

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,11 @@ exports.createPages = ({ graphql, actions }) => {
   createRedirect({
     fromPath: '/newsletter',
     toPath: 'https://www.getrevue.co/profile/sebastien-lorber',
-    isPermanent: true,
+    isPermanent: false,
+  });
+  createRedirect({
+    fromPath: '/sponsor',
+    toPath: 'https://gist.github.com/slorber/4015b388edbe9743048fe98d12604326',
+    isPermanent: false,
   });
 };
