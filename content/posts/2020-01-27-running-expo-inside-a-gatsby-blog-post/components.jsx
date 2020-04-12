648a0027d9f08d375abf9afb56d0f1c59787a03a
slorber <lorber.sebastien@gmail.com>
2020-04-12 21:01:01 +0200

    Add other demos

--- content/posts/2020-01-27-running-expo-inside-a-gatsby-blog-post/components.jsx
+++ content/posts/2020-01-27-running-expo-inside-a-gatsby-blog-post/components.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { Camera } from 'expo-camera';
 import Switch from 'expo-dark-mode-switch';
 import * as Permissions from 'expo-permissions';
 import MobilePhoneView from 'components/MobilePhoneView';
 import AppButton from 'components/designSystem/AppButton';
 import AppRevealView from 'components/designSystem/AppRevealView';
-
-export const ExpoSwitchDemo = () => {
-  const [value, setValue] = React.useState(true);
-  return (
-    <MobilePhoneView style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Switch value={value} onChange={value => setValue(value)} />
-    </MobilePhoneView>
-  );
-};
+import * as Battery from 'expo-battery';
+import ExpoGesturesExample from './ExpoGesturesExample';
+import ExpoImagePickerExample from './ExpoImagePickerExample';
+import Svg, { Circle, Rect } from 'react-native-svg';
+import { Video } from 'expo-av';
 
 export const ExpoCameraDemo = () => {
   const [showCamera, setShowCamera] = useState(false);
@@ -38,6 +34,104 @@ export const ExpoCameraDemo = () => {
           </AppButton>
         </View>
       )}
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoSwitchDemo = () => {
+  const [value, setValue] = React.useState(true);
+  return (
+    <MobilePhoneView style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Switch value={value} onChange={value => setValue(value)} />
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoBatteryDemo = () => {
+  const [battery, setBattery] = useState(undefined);
+
+  useEffect(() => {
+    let canceled = false;
+    const subscription = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+      setBattery(batteryLevel);
+    });
+    Battery.getBatteryLevelAsync().then(batteryLevel => {
+      if (!canceled) setBattery(batteryLevel);
+    });
+    return () => {
+      canceled = true;
+      subscription.remove();
+    };
+  }, []);
+
+  return (
+    <MobilePhoneView style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Battery level = {battery}</Text>
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoGesturesDemo = () => {
+  return (
+    <MobilePhoneView safeAreaPaddingTop={0}>
+      <ExpoGesturesExample />
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoSvgDemo = () => {
+  return (
+    <MobilePhoneView style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="blue"
+          strokeWidth="2.5"
+          fill="green"
+        />
+        <Rect
+          x="15"
+          y="15"
+          width="70"
+          height="70"
+          stroke="red"
+          strokeWidth="2"
+          fill="yellow"
+        />
+      </Svg>
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoVideoDemo = () => {
+  return (
+    <MobilePhoneView
+      safeAreaPaddingTop={0}
+      style={{ alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Video
+        source={{
+          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        rate={1.0}
+        isMuted={true}
+        resizeMode="cover"
+        shouldPlay={true}
+        isLooping={true}
+        style={{ width: '100%' }}
+      />
+    </MobilePhoneView>
+  );
+};
+
+export const ExpoImagePickerDemo = () => {
+  return (
+    <MobilePhoneView
+      style={{ alignItems: 'center', justifyContent: 'center' }}
+    >
+      <ExpoImagePickerExample/>
     </MobilePhoneView>
   );
 };
--- src/components/designSystem/AppButton.jsx
+++ src/components/designSystem/AppButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-native-paper';
 import { useAsyncCallback } from 'react-async-hook';
 
-const AppButton = ({ onPress, children }) => {
+const AppButton = ({ onPress, children, style }) => {
   const asyncCallback = useAsyncCallback(onPress, {
     onError: e => console.error('useAsyncCallback error', e),
   });
@@ -11,7 +11,7 @@ const AppButton = ({ onPress, children }) => {
       onPress={asyncCallback.execute}
       loading={asyncCallback.status === 'loading'}
       mode="contained"
-      style={{ alignSelf: 'center' }}
+      style={[{ alignSelf: 'center' }, style]}
     >
       {children}
     </Button>
