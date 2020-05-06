d03593ee508d7691f4510eb6f94a7b9f9a844caf
slorber <lorber.sebastien@gmail.com>
2020-05-06 17:05:15 +0200

    slides progress

---
+++ slides/gatsby-expo/template.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Template = () => {
+        return (
+                <div>
+                        <div
+                                style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        right: 0,
+                                        padding: 5,
+                                        fontSize: 25,
+                                }}
+                        >
+                                @sebastienlorber
+                        </div>
+                </div>
+        );
+};
+export default Template;
---
+++ slides/gatsby-expo/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+        //space: [0, 16, 32, 64],
+        colors: {
+                //primary: '#f00',
+                //secondary: '#00f',
+        },
+        fontSizes: {
+                pre: '64px',
+                code: '64px',
+                h1: '68px',
+                h2: '56px',
+                h3: '48px',
+                text: '28px',
+                paragraph: '24px',
+        },
+};
+export default theme;
