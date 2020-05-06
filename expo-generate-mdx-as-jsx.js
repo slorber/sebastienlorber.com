788189e354335fd1058140230c9b03737396929c
slorber <lorber.sebastien@gmail.com>
2020-05-06 18:26:16 +0200

    finish pres

--- expo-generate-mdx-as-jsx.js
+++ expo-generate-mdx-as-jsx.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const mdx = require('@mdx-js/mdx');
 const glob = require('glob');
-const grayMatter = require(`gray-matter`);
+const grayMatter = require('gray-matter');
 
 const Encoding = 'utf8';
 
 // Copy of gatsby mdx plugin, also extracting frontmatter variable
 // https://github.com/gatsbyjs/gatsby/blob/561d33e2e491d3971cb2a404eec9705a5a493602/packages/gatsby-plugin-mdx/utils/mdx.js
-const toJSX = source => {
+const toJSX = (source) => {
   const { data, content } = grayMatter(source);
 
-  let code = mdx.sync(content);
+  const code = mdx.sync(content);
 
   return `${code}
 export const frontmatter = ${JSON.stringify(data)};
@@ -18,7 +18,7 @@ export const frontmatter = ${JSON.stringify(data)};
 };
 
 // Regular mdx add images with path as string, but RN needs require("imagePath") to work...
-const imageSrcToRequire = jsx => {
+const imageSrcToRequire = (jsx) => {
   return jsx.replace(
     /"(src|hero)": ?("(\\"|[^"])*\.(jpg|jpeg|png)")/g,
     (match, attribute, value) => {
@@ -33,7 +33,7 @@ const imageSrcToRequire = jsx => {
 const mdxFiles = glob.sync('./content/**/*.mdx');
 console.log('Generating JSX files from MDX articles');
 
-const jsxFiles = mdxFiles.map(mdxFile => {
+const jsxFiles = mdxFiles.map((mdxFile) => {
   const mdxText = fs.readFileSync(mdxFile, Encoding);
   // pragma will be added just after, including the import
   const jsxText = imageSrcToRequire(toJSX(mdxText)).replace(
@@ -74,7 +74,7 @@ export type AppBlogPost = {
 
 export default [
 ${jsxFiles
-  .map(jsxFile => {
+  .map((jsxFile) => {
     return `  require('${jsxFile}')`;
   })
   .join(',\n')}
