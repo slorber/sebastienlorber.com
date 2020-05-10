88cab9992ca1b5fc45da3b3acf901e9ea281c67e
slorber <lorber.sebastien@gmail.com>
2020-05-10 19:31:21 +0200

    add v2

---
+++ slides/gatsby-expo-v2/template.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Template = () => {
+        return (
+                <div
+                        style={{
+                                position: 'absolute',
+                                bottom: 0,
+                                right: 0,
+                                padding: 5,
+                                fontSize: 20,
+                        }}
+                >
+                        @sebastienlorber
+                </div>
+        );
+};
+export default Template;
