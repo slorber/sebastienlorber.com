12905004c4897db08a6ebf5c84f2e0d985d36226
slorber <lorber.sebastien@gmail.com>
2020-05-13 11:57:39 +0200

    improve presentation + implement simple mdx RN test

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -8,15 +8,50 @@ import {
   useRoute,
   DefaultTheme,
   DarkTheme,
+  useNavigation,
 } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useColorMode } from 'theme/useColorMode';
+import { View } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
 const BlogPostScreen = () => {
   const { blogPost } = useRoute().params as { blogPost: AppBlogPost };
   return <AppBlogPostScreen blogPost={blogPost} />;
+};
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ padding: 40 }}>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('SimpleMDX')}
+        style={{ margin: 20 }}
+      >
+        Simple MDX
+      </Button>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('BlogPostList')}
+        style={{ margin: 20 }}
+      >
+        Blog Post List
+      </Button>
+    </View>
+  );
+};
+
+const SimpleMDXScreen = () => {
+  const navigation = useNavigation();
+  const MDXComponent = require('./content/test/simpleMDX.mdx').default;
+  return (
+    <View style={{ padding: 40 }}>
+      <MDXComponent />
+    </View>
+  );
 };
 
 const AppNavigator = () => {
@@ -26,6 +61,20 @@ const AppNavigator = () => {
       theme={colorMode === 'dark' ? DarkTheme : DefaultTheme}
     >
       <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name="SimpleMDX"
+          component={SimpleMDXScreen}
+          options={{
+            title: 'Simple MDX',
+          }}
+        />
         <Stack.Screen
           name="BlogPostList"
           component={AppBlogPostListScreen}
